d1e9a61a6700f321d4c35af762961d41d98e5b51
Mouayad Kh <36438758+mou2ayad@users.noreply.github.com>
2019-02-09 15:22:06 +0800

    Update Balanced_Brackets.cs

--- Stacks and Queues/Balanced_Brackets.cs
+++ Stacks and Queues/Balanced_Brackets.cs
@@ -1,5 +1,21 @@
-class  Balanced_Brackets {
-static string isBalanced(string s) {
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the isBalanced function below.
+    static string isBalanced(string s) {
          if (s.Length % 2 != 0)
                 return "NO";
             Stack<char> stl = new Stack<char>();
@@ -27,4 +43,21 @@ static string isBalanced(string s) {
                 return true;
             return false;
         }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            string s = Console.ReadLine();
+
+            string result = isBalanced(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
 }
